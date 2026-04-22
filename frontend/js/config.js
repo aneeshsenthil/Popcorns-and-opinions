ee03dbd4f8509ff89bb3193af282b4f179347646
Aneesh senthil <aneesh.23mis7150@vitapstudent.ac.in>
2026-04-23 00:38:25 +0530

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,7 +1,4 @@
 // config.js
-
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY || "db536043007cfb075f36f62e669838a2";
-
+const API_KEY = "db536043007cfb075f36f62e669838a2";
 const BASE_URL = "https://api.themoviedb.org/3";
-
-const IMAGE_BASE = import.meta.env.VITE_IMAGE_BASE || "https://image.tmdb.org/t/p/w500";
+const IMAGE_BASE = "https://image.tmdb.org/t/p/w500";
